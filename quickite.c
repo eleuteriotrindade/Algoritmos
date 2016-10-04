63e286ee196cfb2fea9f8c753a6f8d48a272812c
Victor Eleuterio Trindade <eleuteriotrindade@gmail.com>
2016-10-04 16:50:15 -0300

    Add files via upload

--- quickite.c
+++ quickite.c
@@ -1,64 +1,108 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
-//algoritmo retirado de http://www.geeksforgeeks.org/iterative-quick-sort/
-
-void Quick(int *vetor, int inicio, int fim){
-
-	int pivo, aux, i, j, meio;
-
-	i = inicio;
-	j = fim;
-
-	meio = (int)((i + j) / 2);
-	pivo = vetor[meio];
-
-	do{
-		while (vetor[i] < pivo) i = i + 1;
-		while (vetor[j] > pivo) j = j - 1;
-
-		if (i <= j){
-			aux = vetor[i];
-			vetor[i] = vetor[j];
-			vetor[j] = aux;
-			i = i + 1;
-			j = j - 1;
-		}
-	} while (j > i);
-
-	if (inicio < j) Quick(vetor, inicio, j);
-	if (i < fim) Quick(vetor, i, fim);
-
+#include <time.h> 
+// A utility function to swap two elements
+void swap ( int* a, int* b )
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
-
-int main(){
-
-	clock_t Ticks[2];
-	Ticks[0] = clock();
-
-	int vetor[1000], tamanho = 1000;
-
-	srand((unsigned)time(NULL));
-
-	for (int k = 0; k < tamanho; k++)
-	{
-		vetor[k] = rand() % 100000;
-	}
-
-	Quick(vetor, 0, 999);
-	
-	printf("Vetor ordenado:\n");
-	for (int i = 0; i < 1000; i++)
-	{
-		printf("%d ", vetor[i]);
-	}
-
-	Ticks[1] = clock();
-	double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
-	printf("\nTempo gasto: %g ms.", Tempo);
-	getchar();
-
-	return 0;
+ 
+/* This function is same in both iterative and recursive*/
+int partition (int arr[], int l, int h)
+{
+    int x = arr[h];
+    int i = (l - 1);
+ 
+    for (int j = l; j <= h- 1; j++)
+    {
+        if (arr[j] <= x)
+        {
+            i++;
+            swap (&arr[i], &arr[j]);
+        }
+    }
+    swap (&arr[i + 1], &arr[h]);
+    return (i + 1);
 }
+ 
+/* A[] --> Array to be sorted, 
+   l  --> Starting index, 
+   h  --> Ending index */
+void quickSortIterative (int arr[], int l, int h)
+{
+    // Create an auxiliary stack
+    int stack[ h - l + 1 ];
+ 
+    // initialize top of stack
+    int top = -1;
+ 
+    // push initial values of l and h to stack
+    stack[ ++top ] = l;
+    stack[ ++top ] = h;
+ 
+    // Keep popping from stack while is not empty
+    while ( top >= 0 )
+    {
+        // Pop h and l
+        h = stack[ top-- ];
+        l = stack[ top-- ];
+ 
+        // Set pivot element at its correct position
+        // in sorted array
+        int p = partition( arr, l, h );
+ 
+        // If there are elements on left side of pivot,
+        // then push left side to stack
+        if ( p-1 > l )
+        {
+            stack[ ++top ] = l;
+            stack[ ++top ] = p - 1;
+        }
+ 
+        // If there are elements on right side of pivot,
+        // then push right side to stack
+        if ( p+1 < h )
+        {
+            stack[ ++top ] = p + 1;
+            stack[ ++top ] = h;
+        }
+    }
+}
+ 
+// A utility function to print contents of arr
+void printArr( int arr[], int n )
+{
+    int i;
+    for ( i = 0; i < n; ++i )
+        printf( "%d ", arr[i] );
+}
+ 
+// Driver program to test above functions
+int main()
+{
 
+    clock_t Ticks[2];
+    Ticks[0] = clock();
+
+    int arr[1000000];
+    int n = sizeof( arr ) / sizeof( *arr );
+    int controle = 0;
+
+    srand( (unsigned)time(NULL) );
+
+    for(controle = 0 ; controle < n ; controle++)
+    {
+        arr[controle] = rand () % 10000;
+    }
+
+
+    quickSortIterative( arr, 0, n - 1 );
+    printArr( arr, n );
+    Ticks[1] = clock();
+    double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+    printf("\nTempo gasto: %g ms.", Tempo);
+    getchar();
+    return 0;
+}
